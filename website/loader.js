bd02f082b63ff31483f4bb4dd2943d148f6f4973
Ian Sparks <isparks@trialgrid.com>
2021-04-04 18:18:21 +0100

    CSS animations

---
+++ website/loader.js
@@ -0,0 +1,36 @@
+// Loader creates loading html and injects it into the page
+
+function start_loader(message) {
+   const loader = document.createElement("div");
+    Object.assign(loader, {
+      id: 'loading'
+    })
+
+    const psychology = document.createElement("div");
+    Object.assign(psychology, {
+      id: 'psychology',
+       className : 'pulsing'
+    })
+
+    const loading_icon = document.createElement("i");
+    Object.assign(loading_icon, {
+      id: 'loading_icon',
+       className : 'fa fa-circle-o-notch fa-spin fa-fw'
+    })
+    psychology.appendChild(loading_icon);
+
+    const loading_message = document.createElement("span");
+    Object.assign(loading_message, {
+      id: 'loading_message',
+    })
+    loading_message.innerHTML = message;
+    psychology.appendChild(loading_message);
+
+   loader.appendChild(psychology);
+   document.body.appendChild(loader);
+}
+
+
+function update_loader_message(message) {
+    document.getElementById('loading_message').innerHTML = message;
+}
